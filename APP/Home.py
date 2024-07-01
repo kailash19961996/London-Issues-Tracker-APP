994d83d3699c8ae624ad97023a43502d087ad1d3
kailash19961996 <kylasben@gmail.com>
2024-07-01 18:32:28 +0100

    comments fixing

--- APP/Home.py
+++ APP/Home.py
@@ -115,7 +115,9 @@ if uploaded_file is not None:
         sample_image_path = 'APP/sample_image/graffiti.jpg'  # Path to your sample image
         if os.path.exists(sample_image_path):
             with open(sample_image_path, "rb") as file:
-                st.write(f"NOTE : As of now, the model is designed to detect only these 6 categories: GRAFFITI, GARBAGE, BROKEN WINDOW, GREEN SPACES, PUBLIC BUILDINGS, SPORTS AND SOCIAL EVENTS. If you are not sure what to upload, download the same image below, else, proceed with the commenting.")
+                st.write(f"NOTE : As of now, the model is designed to detect only these 6 categories: ")
+                st.write(f"GRAFFITI, GARBAGE, BROKEN WINDOW, GREEN SPACES, PUBLIC BUILDINGS, SPORTS AND SOCIAL EVENTS")
+                st.write(f"If you are not sure what to upload, download the same image below, else, proceed with the commenting.")
                 btn = st.download_button(
                     label="Download a sample image",
                     data=file,
