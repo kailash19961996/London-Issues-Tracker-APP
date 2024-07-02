ff8dbfa02885fffc5a700109ed06549f377fbf3b
Kailash Balasubramaniyam <123597753+kailash19961996@users.noreply.github.com>
2024-07-02 01:24:31 +0100

    Update Home.py

--- APP/Home.py
+++ APP/Home.py
@@ -21,11 +21,6 @@ col1, col2 = st.columns([1, 5])
 with col1:
     st.image(logo, width=100)  # Adjust width as needed
 with col2:
-    st.markdown("""
-    <div style='text-align: center;'>
-    <h1> < London Issue Tracker > </h1>
-    </div>
-    """, unsafe_allow_html=True)
     st.title("London Issue Tracker")
 
 st.markdown("""
