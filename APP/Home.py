f60897ef4d4728c09976ee3688e06fcc7f218787
Kailash Balasubramaniyam <123597753+kailash19961996@users.noreply.github.com>
2024-07-04 00:34:34 +0100

    Update Home.py

--- APP/Home.py
+++ APP/Home.py
@@ -40,6 +40,7 @@ st.markdown(f"""
     <div class="centered">
         <img src="data:image/png;base64,{logo_base64}" alt="Logo">
         <h1>AI London Issue Tracker</h1>
+        <h3>AI London Issue Tracker</h3>
     </div>
     """, unsafe_allow_html=True)
 
