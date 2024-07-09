ebbcddaa2a6e87b93679d33320adde469e3764e4
Kailash Balasubramaniyam <123597753+kailash19961996@users.noreply.github.com>
2024-07-09 22:42:11 +0100

    Update APP Documentation.py

--- APP/pages/APP Documentation.py
+++ APP/pages/APP Documentation.py
@@ -1,1 +1,15 @@
+st.write(f"How This App is Useful:")
 
+st.write(f"1️⃣ For Reporters: If you notice something wrong or have a suggestion for your community, you can easily report it here. On hashtag#Home page, upload your image , your comment, and location, then submit.")
+
+st.write(f"2️⃣ For the Audience: Even if you don't have reports to submit, you can explore all reports on our interactive hashtag#Maps and hashtag#Graphs page to stay informed about what's happening around you.")
+
+st.write(f"3️⃣ For Authorities: Policymakers and politicians can access a summary of all reports on the hashtag#AISummary page to address and fix issues within the community, promoting transparency and accountability. 🗺️📊")
+
+st.write(f"The Story Behind the App:")
+
+st.write(f"The London Issues Tracker was created during a hackathon organized by FutureLondon.org, with contributions from passionate policymakers and tech enthusiasts Joseph Reeve, and Nathan Young. This project is a testament to what can be achieved when technology and community spirit come together. Special thanks to everyone who contributed to making this vision a reality! Mark Hodierne💡👩‍💻👨‍💻")
+
+st.write(f"")
+
+st.write(f"")
